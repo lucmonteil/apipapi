c3e227ba9d80da87cb78b84a990ba2132ba60c69
gildas fremont <gildas.fremont@gmail.com>
2016-08-22 18:21:02 +0200

    reception de sms ok, enregistrment des messages ok

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,21 @@
 class MessagesController < ApplicationController
 
  #vérifier le skip_before_filter (skipp la vérification de l'auth token)
- skip_before_filter :verify_authenticity_token
+ skip_before_action :verify_authenticity_token
 
  #skip l'auth token pour Devise
- skip_before_filter :authenticate_user!, :only => "reply"
+ skip_before_action :authenticate_user!, :only => "reply"
 
   def reply
     message_body = params["Body"]
     from_number = params["From"]
+    @user = User.find_by_phone_number(from_number)
+    Message.create(body: message_body, user: @user)
     boot_twilio
     sms = @client.messages.create(
       from: ENV['TWILIO_NUMBER'],
-      to: '+33665579224',
-      body: "Hello there, thanks for texting me. Your number is #{from_number}."
+      to: from_number,
+      body: "Hello world mothafoka!"
     )
 
   end
