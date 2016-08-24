765a26f83d8c8af98fb43d4eae78b64f6f7e2807
gildas fremont <gildas.fremont@gmail.com>
2016-08-24 13:06:38 +0200

    sauvegarde avant de modifier la DB

--- app/services/message_parser.rb
+++ app/services/message_parser.rb
@@ -4,7 +4,7 @@ class MessageParser
     @message_body = message_body
   end
 
-  def parse_for_address
+  def parse_for_start_and_end_address
     split = @message_body.split(";")
 
     start_address = Address.create(query: split[0])
@@ -14,6 +14,18 @@ class MessageParser
       start_address_reverse: Geocoder.search("#{start_address.latitude},#{start_address.longitude}")[0],
       start_latitude: start_address.latitude,
       start_longitude: start_address.longitude,
+      end_address: end_address,
+      end_address_reverse: Geocoder.search("#{end_address.latitude},#{end_address.longitude}")[0],
+      end_latitude: end_address.latitude,
+      end_longitude: end_address.longitude,
+    }
+  end
+
+  def parse_for_end_address
+
+    end_address = Address.create(query: @message_body)
+
+    return {
       end_address: end_address,
       end_address_reverse: Geocoder.search("#{end_address.latitude},#{end_address.longitude}")[0],
       end_latitude: end_address.latitude,
