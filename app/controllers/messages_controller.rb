a4defad0956f36d6c8344519c19603ba92c8b4b4
gildas fremont <gildas.fremont@gmail.com>
2016-08-24 16:14:49 +0200

    nice

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
     @sender = true
     # les messages sont des tests
     @test = true
-    @message_body = "Test: " + params[:message][:body]
+    @message_body = params[:message][:body]
     @phone_number = params[:message][:user]
     # création de l'utilisateur s'il n'existe pas
     # sauvegarde du message
@@ -37,66 +37,15 @@ class MessagesController < ApplicationController
 
   private
 
-  def create_user
-    @user = User.create({
-              email: "#{@phone_number}@apipapi.com",
-              password: Random.new_seed,
-              phone_number: @phone_number,
-              # le nom de l'utilisateur est set à UNKNOWN pour l'utiliser dans les vues
-              first_name: "UNKNOWN"
-            })
-  end
-
-  def create_message
-    Message.create(body: @message_body, user: @user, sender: @sender)
-  end
-
   def set_user_create_message_parse_and_point
-    create_user unless = User.find_by_phone_number(@phone_number)
+    create_user unless @user = User.find_by_phone_number(@phone_number)
     create_message
     parse_and_point
   end
 
   # interprète le corps parsé et renvoit vers la bonne méthode
   def parse_and_point
-    # création d'une course (qui ne fait rien pour l'instant)
-    create_ride unless @ride
-    # check si on attends une réponse, ici
-    # TODO créer la colonne waiting_for_sms et set la valeur à false quand
-    # le service a été rendu ou qu'un temps X c'est passé depuis le dernier sms
-    if ❗@ride.waiting_for_sms && @end_address_reverse
-      @parsing = MessageParser.new(@message_body).parse_for_end_address
-    else
-      @parsing = MessageParser.new(@message_body).parse_for_start_and_end_address
-    end
-
-    # un peu de ménage pour plus de lisibilité
-    @start_address = @parsing[:start_address]
-    @end_address = @parsing[:end_address]
-    # ici les lat,lng réinterpretées par géocoder
-    @start_address_reverse = @parsing[:start_address_reverse]
-    @end_address_reverse = @parsing[:end_address_reverse]
-
-    # réponses en fonction de la situation
-    if @start_address_reverse && @end_address_reverse
-      # l'échange est terminé
-      ❗@ride.waiting_for_sms = false
-      @message_body = "Un Uber arrive au #{@start_address_reverse.formatted_address} pour le #{@end_address_reverse.formatted_address}"
-      @ride.start_address_id = @start_address.id
-      @ride.end_address_id = @end_address.id
-    elsif @start_address_reverse
-      ❗@ride.waiting_for_sms = false
-      @message_body = "Un Uber arrive au #{@start_address_reverse.formatted_address}, pourriez-vous donner l'adresse d'arrivée au chauffeur ?"
-      @ride.start_address_id = @start_address.id
-    elsif @end_address_reverse
-      # l'échange n'est pas terminé, il y a besoin d'un nouveau sms
-      ❗@ride.waiting_for_sms = true
-      @message_body = "Pourriez-vous renvoyer l'adress de départ avec plus de détail (code postal par exemple) ?"
-      @ride.end_address_id = @end_address.id
-    else
-      ❗@ride.waiting_for_sms = true
-      @message_body = "Pourriez-vous renvoyer les adresses, avec plus de détail..."
-    end
+    @reply_message_body = MessageParser.new(@message_body, @user).point
 
     # en cas de test...
     fake_reply if @test
@@ -120,18 +69,31 @@ class MessagesController < ApplicationController
     sms = @client.messages.create(
             from: @apipapi_phone_number,
             to: @phone_number,
-            body: @message_body
+            body: @reply_message_body
           )
   end
 
   #sauvegarde de la réponse sans l'envoyer
   def fake_reply
     @sender = false
-    @message_body = "Test : " + @message_body
+    @message_body = @reply_message_body
   end
 
-  # création du "service ride"
-  def create_ride
-    @ride = Ride.create(user: @user, status: "pending")
+  def create_user
+    @user = User.create({
+              email: "#{@phone_number}@apipapi.com",
+              password: Random.new_seed,
+              phone_number: @phone_number,
+              # le nom de l'utilisateur est set à UNKNOWN pour l'utiliser dans les vues
+              first_name: "UNKNOWN"
+            })
+  end
+
+  def create_message
+    if @test
+    Message.create(body: "Test : " + @message_body, user: @user, sender: @sender)
+    else
+    Message.create(body: @message_body, user: @user, sender: @sender)
+    end
   end
 end
--- app/services/message_parser.rb
+++ app/services/message_parser.rb
@@ -1,35 +1,81 @@
 class MessageParser
 
-  def initialize(message_body)
+  def initialize(message_body, user)
     @message_body = message_body
+    @user = user
   end
 
+  def point
+    create_request unless @request
+    return ride
+  end
+
+  def ride
+    # création d'une course (qui ne fait rien pour l'instant)
+    waiting_sms = @request.wait_message
+    # check si on attends une réponse, ici
+    # TODO créer la colonne waiting_for_sms et set la valeur à false quand
+    # le service a été rendu ou qu'un temps X c'est passé depuis le dernier sms
+    if waiting_sms && @end_address_nice
+      parse_for_end_address
+    else
+      parse_for_start_and_end_address
+    end
+
+    # réponses en fonction de la situation
+    if @start_address_nice && @end_address_nice
+      # l'échange est terminé
+      waiting_sms = false
+      return "Un Uber arrive au #{@start_address_nice} pour le #{@end_address_nice}"
+    elsif @start_address_nice
+      waiting_sms = false
+      return "Un Uber arrive au #{@start_address_nice}, pourriez-vous donner l'adresse d'arrivée au chauffeur ?"
+    elsif @end_address_nice
+      # l'échange n'est pas terminé, il y a besoin d'un nouveau sms
+      waiting_sms = true
+      return "Pourriez-vous renvoyer l'adress de départ avec plus de détail (code postal par exemple) ?"
+    else
+      waiting_sms = true
+      return "Pourriez-vous renvoyer les adresses, avec plus de détail..."
+    end
+  end
+
+  private
+
+  # création du "service ride"
+  def create_request
+    create_ride
+    @request = Request.create(wait_message: true, service: @ride)
+  end
+
+  # création du "service ride", à balancer dans Uber_service
+  def create_ride
+    @ride = Ride.create(status: "pending")
+  end
+
+  # ces methodes seront renvoyées dans RECAST.AI
   def parse_for_start_and_end_address
     split = @message_body.split(";")
 
-    start_address = Address.create(query: split[0])
-    end_address = Address.create(query: split[1])
-    return {
-      start_address: start_address,
-      start_address_reverse: Geocoder.search("#{start_address.latitude},#{start_address.longitude}")[0],
-      start_latitude: start_address.latitude,
-      start_longitude: start_address.longitude,
-      end_address: end_address,
-      end_address_reverse: Geocoder.search("#{end_address.latitude},#{end_address.longitude}")[0],
-      end_latitude: end_address.latitude,
-      end_longitude: end_address.longitude,
-    }
+    @start_address = split[0]
+    if address = Address.create(query: @start_address)
+      @start_latitude = address.latitude
+      @start_longitude = address.longitude
+      @start_address_nice = Geocoder.search("#{@start_latitude},#{@start_longitude }")[0].formatted_address
+    end
+
+    @end_address = split[1]
+    if address = Address.create(query: @end_address)
+      @end_latitude = address.latitude
+      @end_longitude = address.longitude
+      @end_address_nice = Geocoder.search("#{@end_latitude},#{@end_longitude}")[0].formatted_address
+    end
   end
 
   def parse_for_end_address
-
-    end_address = Address.create(query: @message_body)
-
-    return {
-      end_address: end_address,
-      end_address_reverse: Geocoder.search("#{end_address.latitude},#{end_address.longitude}")[0],
-      end_latitude: end_address.latitude,
-      end_longitude: end_address.longitude,
-    }
+    address = Address.create(query: @message_body)
+    @end_latitude = address.latitude
+    @end_longitude = address.longitude
+    @end_address_nice = Geocoder.search("#{@end_latitude},#{@end_longitude}")[0]
   end
 end
