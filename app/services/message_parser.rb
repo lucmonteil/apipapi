97f4e77272746fd405beb2b2c62445315daec8be
gildasfremont <gildas.fremont@gmail.com>
2016-09-01 13:04:39 +0200

    Revert "fixed bug on message parser"

--- app/services/message_parser.rb
+++ app/services/message_parser.rb
@@ -14,10 +14,6 @@ class MessageParser
 
   def reply
 
-    error = "Je n'ai pas compris votre demande. Pour le moment " \
-            "nous proposons des courses UBER. Demandez une estimation en m'envoyant " \
-            "votre adresse de départ (avec la ville) et votre adresse d'arrivée."
-
     if @intention == "say-hi"
       sentences = @parsed_message.sentences
       if sentence = sentences.detect { |sentence| sentence.entities.detect {|entity| entity.name == "person" } }
@@ -68,6 +64,10 @@ class MessageParser
     else
       return error
     end
+
+    error = "Je n'ai pas compris votre demande. Pour le moment " \
+            "nous proposons des courses UBER. Demandez une estimation en m'envoyant " \
+            "votre adresse de départ (avec la ville) et votre adresse d'arrivée."
   end
 
   private
